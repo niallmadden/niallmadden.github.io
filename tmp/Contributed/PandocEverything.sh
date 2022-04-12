7183cf8f97c94bf25c6a5db5b6060c18648425e2
niallmadden <Niall.Madden@NUIGalway.ie>
2022-04-12 17:44:20 +0100

    del tmp

--- tmp/Contributed/PandocEverything.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in *.tex;
-do
-   echo "Pandocing " "$f" 
-   pandoc -f latex --mathjax --lua-filter=dollar-math.lua -t html "$f" -s -o "${f%.tex}.html";
-done
--- tmp/Contributed/dollar-math.lua
+++
@@ -1,6 +0,0 @@
-if not FORMAT:match('tex$') then return {} end
-
-function Math (m)
-  local delimiter = m.mathtype == 'InlineMath' and '$' or '$$'
-  return pandoc.RawInline('tex', delimiter .. m.text .. delimiter)
-end
