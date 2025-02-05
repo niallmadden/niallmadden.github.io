f11902c5935902ae1dc76bf15a568a1062d4a863
niallmadden <Niall.Madden@NUIGalway.ie>
2025-02-05 22:56:36 +0000

    Lab 2

--- 2425-CS319/lab2/Lab2-Q3.cpp
+++ 2425-CS319/lab2/Lab2-Q3.cpp
@@ -10,27 +10,27 @@
 #include <cmath> // for pow
 #include <limits> // to check the value
 
-float f(float x) {return(exp(-x));}
+float f(float x) {return(exp(-x));} // function to differentiate
 
 int main(void)
 {
    std::cout << std::endl <<
-     "---------------------------------\n" << 
-     "|   CS319  Lab 2, Q3,  Feb 2025   |\n" <<
-     "|   Numerical Differentiation     |\n" <<
-     "---------------------------------\n";
+     " ---------------------------------\n" << 
+     "|  CS319  Lab 2, Q3,  Feb 2025    |\n" <<
+     "|  Numerical Differentiation      |\n" <<
+     "|  This version by: Niall Madden  |\n" <<
+     " ---------------------------------\n";
 
-   float df0_true = -1.0, df0_approx2, df0_approx3;
-   float x0=0.0, h=1.0e-1;
-   df0_approx2 = (f(x0+h)-f(x0))/h; // two point
-   df0_approx3 = (f(x0+h)-f(x0-h))/(2*h); // three point
+   double x0 = 1.0; // point to differentiate at
+   double Df_true = -exp(-x0), Df_approx;
+   std::cout << std::fixed << std::showpoint;
+   std::cout << std::setprecision(8);
+   std::cout << "\t True:\t " << Df_true << std::endl;
+   std::cout << "     h     |   2-point   |   Error "<< std::endl;
 
-   std::cout << std::setprecision(6);
-   std::cout << "Derivative (true):\t " << df0_true << std::endl;
-   std::cout << "Derivative (2-point):\t " << df0_approx2;
-   std::cout << "\tError:\t " << fabs(df0_true-df0_approx2) << std::endl;
-   std::cout << "Derivative (3-point):\t " <<  df0_approx3;
-   std::cout << "\tError:\t " << fabs(df0_true-df0_approx3) << std::endl;
-
-   return(0);
+   float h=pow(2.0,-2);
+   Df_approx = (f(x0+h)-f(x0))/h; // two point
+   double error = fabs(Df_true-Df_approx);
+   std::cout << h << " | " << std::setw(11) << Df_approx << " | "
+	       << error << std::endl;
 }
