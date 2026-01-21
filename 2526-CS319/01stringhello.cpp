a9c91500fa8e419e2b97916428ac808ba388cb26
niallmadden <Niall.Madden@NUIGalway.ie>
2026-01-21 15:00:42 +0000

    CS319 W02

---
+++ 2526-CS319/01stringhello.cpp
@@ -0,0 +1,12 @@
+// CS319 -- Scientific Computing
+// 01stringhello.cpp
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string message="Well, hello again.";
+  std::cout << message << "\n";
+  return(0);
+}
+ 
---
+++ 2526-CS319/04Input.cpp
@@ -0,0 +1,19 @@
+// 04Input.cpp
+// Demonstrating use of "cin" for input.
+
+#include <iostream>
+#include <iomanip> // needed for setprecision
+int main()
+{
+  const double StirlingToEuro=1.16541; // Correct 17/01/2024
+  double Stirling;
+  std::cout << "Input amount in Stirling: ";
+  std::cin >> Stirling;
+  std::cout << "That is worth "
+	    << Stirling*StirlingToEuro << " Euros\n";
+  std::cout << "That is worth " << std::fixed
+	    << std::setprecision(2) << "\u20AC"
+	    << Stirling*StirlingToEuro << std::endl;
+  return(0);
+}
+ 
