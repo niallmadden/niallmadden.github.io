40802b4d7c5b39be101606fcede6bc59dc4b5a43
niallmadden <Niall.Madden@NUIGalway.ie>
2026-02-04 15:17:37 +0000

    CS319 W04

---
+++ 2526-CS319/Week04/01MemoryAddresses.cpp
@@ -0,0 +1,26 @@
+// Compare memory addresses
+
+#include <iostream>
+
+void Address(int i);
+
+int main(void)
+{
+  int i=12;
+  std::cout << "main: Value stored in i: " << i << '\n';
+  std::cout << "main: address of i: " << &i << "\n\n";
+  Address(i);
+  std::cout << "main: Value stored in i: " << i << '\n';
+  std::cout << "main: address of i: " << &i << '\n';
+  return(0);
+}
+
+void Address(int i)
+{
+  std::cout << "Address: Value stored in i: " << i << '\n';
+  std::cout << "Address: address of i: " << &i << '\n';
+  i+=10; // Change value of i
+  std::cout << "Address: New val stored in i: " << i << '\n';
+  std::cout << "Address: address of i: " << &i << "\n\n";
+}  
+  
