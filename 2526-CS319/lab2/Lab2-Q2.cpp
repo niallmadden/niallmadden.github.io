a1c7e97f11708908c47b6d41842709bdab7d9f9e
niallmadden <Niall.Madden@NUIGalway.ie>
2026-02-04 15:25:10 +0000

    CS319 lab 2 examples

---
+++ 2526-CS319/lab2/Lab2-Q2.cpp
@@ -0,0 +1,46 @@
+/* FILE  : Lab2-Q1.cpp   
+   Author: Niall Madden (Email: Niall.Madden@UniversityOfGalway.ie)
+   Date  : Feb 2026
+   What  : eps mach for doubles
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <cmath> // for pow
+#include <limits> // to check the value
+
+int main(void)
+{
+   std::cout << std::endl <<
+     "---------------------------------\n" << 
+     "|   CS319  Lab 2, Q1,  Feb 2025   |\n" <<
+     "|       Investigate doubles  \n" <<
+     "---------------------------------\n";
+
+   // Machine epsilon for doubles
+   double epsilon=1.0;
+   double f=1.0,  g = f+epsilon;
+   
+   int n=0;
+   clock_t start=clock();
+   while ( g > 1.0  )
+   {
+     n++;
+     epsilon/=2.0;
+     g = f+epsilon;
+     // std::cout << "eps=" << epsilon << std::endl;
+   }
+   double diff = (double)(clock()-start);
+   double diff_seconds = diff/CLOCKS_PER_SEC;
+   std::cout << std::setprecision(16);
+   std::cout << "(double) eps_mach:\t " << epsilon*2 << std::endl;
+   std::cout << "Value from <limits>:\t "
+	     << std::numeric_limits<double>::epsilon() << std::endl;
+   std::cout << std::setprecision(4);
+   std::cout << "That took " << n << " iterations, so eps_mach is 2^"
+	     << (-n) << "=" << pow(2,-n);
+   std::cout << " (Computation took " << diff_seconds
+        << "s)."  << std::endl;
+   return(0);
+}
